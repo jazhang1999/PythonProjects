b924c99f2ebc43ba8ddba496c61bee4991fcfb06
jazhang1999 <jazhang1999@gmail.com>
2019-03-21 20:25:11 -0400

    Adding a python project that processes a file for its single digit numbers

---
+++ processFileNumbers/Assignment_2.1.py
@@ -0,0 +1,18 @@
+import re
+#test text was renamed textSample.txt, while actual text was renamed textActual.txt
+#handle = open('textSample.txt') - test before submitting
+handle = open('textActual.txt')
+
+masterList = []
+for line in handle:
+    numsInLine = re.findall('[0-9]+', line)
+    masterList = masterList + numsInLine
+    numsInLine = []
+
+grandSum = 0
+for number in masterList:
+    grandSum = grandSum + int(number)
+
+#Find the answer, and check to see all numerical values are accounted for
+print(grandSum)
+print(len(masterList))
